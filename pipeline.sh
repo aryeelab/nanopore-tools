bdceda6e1c57682bcb2117490960690657c457d1
Martin Aryee <martin.aryee@gmail.com>
2019-02-22 01:15:58 -0500

    Added clarifying echo statements to pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,17 +1,18 @@
-sample_id=test-sample-1
-fast5_zip=test_data/test-sample-1.zip
-
+run_id=test-run-1
+fast5_zip=test_data/test-run-1.zip
 flowcell=FLO-MIN106      
 kit=SQK-RBK004
 
 
 # Unzip fast5
 fast5_path=`jar tf $fast5_zip | grep 'fast5/$'` # find path to fast5 dir within zip
+echo "Unzipping $fast5_zip"
 jar -xf $fast5_zip
+echo "Moving $fast5_path to `basename $fast5_path`"
 mv $fast5_path .
 
 
-# Albacore
+# Albacore basecall and demultiplex
 source /apps/lab/aryee/pyenv/versions/nanopore/bin/activate
 read_fast5_basecaller.py -f  $flowcell -k $kit -t 1 -r -i  fast5 -o fastq -q 0 -s albacore
 
