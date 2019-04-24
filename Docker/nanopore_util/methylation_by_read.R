0d9e3c81421c52525fdf2d2ad7e2fb0d40d7a262
Martin Aryee <martin.aryee@gmail.com>
2019-04-24 10:07:38 -0400

    Allow methylation_by_read.R to handle zero line input files

--- Docker/nanopore_util/methylation_by_read.R
+++ Docker/nanopore_util/methylation_by_read.R
@@ -1,5 +1,5 @@
 library(readr, lib.loc="/usr/local/lib/R/site-library")
-library(dplyr, lib.loc="/usr/local/lib/R/site-library")
+library(readr, lib.loc="/usr/local/lib/R/site-library")
 library(stringr, lib.loc="/usr/local/lib/R/site-library")
 
 
@@ -7,40 +7,46 @@ infile <-commandArgs(TRUE)[1] #"/data/aryee/sowmya/ctc_nanopore/test-run-1__barc
 outfile_by_read <- commandArgs(TRUE)[2]
 input <- read_tsv(infile)
 
+if (nrow(input)==0) {
+    message("Warning: No lines in the input file")
+    file.create(outfile_by_read)
+} else {
+    input$log_lik_ratio_repeated <- as.character(lapply(mapply(rep, input$log_lik_ratio, input$num_motifs), paste, collapse=","))
 
-input$log_lik_ratio_repeated <- as.character(lapply(mapply(rep, input$log_lik_ratio, input$num_motifs), paste, collapse=","))
+    input$num_methylated_2.5_thresh <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) > threshold)) }, threshold = 2.5))
+    input$num_UNmethylated_2.5_thresh <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) < threshold)) }, threshold = -2.5))
 
-input$num_methylated_2.5_thresh <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) > threshold)) }, threshold = 2.5))
-input$num_UNmethylated_2.5_thresh <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) < threshold)) }, threshold = -2.5))
-
-input$num_methylated_0_thresh <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) > threshold)) }, threshold = 0))
-input$num_UNmethylated_0_thresh <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) < threshold)) }, threshold = 0))
-
-
-input$num_motifs_WCGW <- str_count(input$sequence, pattern = "[A,T]CG[A,T]")
-input$log_lik_ratio_repeated_WCGW <- as.character(lapply(mapply(rep, input$log_lik_ratio, input$num_motifs_WCGW), paste, collapse=","))
-
-input$num_methylated_2.5_thresh_WCGW <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated_WCGW, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) > threshold)) }, threshold = 2.5))
-input$num_UNmethylated_2.5_thresh_WCGW <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated_WCGW, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) < threshold)) }, threshold = -2.5))
-
-input$num_methylated_0_thresh_WCGW <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated_WCGW, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) > threshold)) }, threshold = 0))
-input$num_UNmethylated_0_thresh_WCGW <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated_WCGW, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) < threshold)) }, threshold = 0))
+    input$num_methylated_0_thresh <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) > threshold)) }, threshold = 0))
+    input$num_UNmethylated_0_thresh <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) < threshold)) }, threshold = 0))
 
 
+    input$num_motifs_WCGW <- str_count(input$sequence, pattern = "[A,T]CG[A,T]")
+    input$log_lik_ratio_repeated_WCGW <- as.character(lapply(mapply(rep, input$log_lik_ratio, input$num_motifs_WCGW), paste, collapse=","))
 
-input %>%
-  group_by(read_name, chromosome) %>%
-  summarize(start = min(start),
-            end = max(end),
-#            log_lik_ratio_collapsed = paste(log_lik_ratio_repeated, collapse=","),
-            total_CGs = sum(num_motifs),
-            total_CGs_methylated_2.5_thresh = sum(num_methylated_2.5_thresh),
-            total_CGs_methylated_0_thresh = sum(num_methylated_0_thresh),
-	          fraction_CGs_methylated_2.5_thresh = sum(num_methylated_2.5_thresh)/sum(num_motifs),
-      	    fraction_CGs_methylated_0_thresh = sum(num_methylated_0_thresh)/sum(num_motifs),
-            total_WCGW = sum(num_motifs_WCGW),
-            total_WCGW_methylated_2.5_thresh = sum(num_methylated_2.5_thresh_WCGW),
-            total_WCGW_methylated_0_thresh = sum(num_methylated_0_thresh_WCGW),
-            fraction_WCGW_methylated_2.5_thresh = sum(num_methylated_2.5_thresh_WCGW)/sum(num_motifs_WCGW),
-            fraction_WCGW_methylated_0_thresh = sum(num_methylated_0_thresh_WCGW)/sum(num_motifs_WCGW)) %>%
-  write_tsv(path=outfile_by_read)
+    input$num_methylated_2.5_thresh_WCGW <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated_WCGW, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) > threshold)) }, threshold = 2.5))
+    input$num_UNmethylated_2.5_thresh_WCGW <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated_WCGW, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) < threshold)) }, threshold = -2.5))
+
+    input$num_methylated_0_thresh_WCGW <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated_WCGW, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) > threshold)) }, threshold = 0))
+    input$num_UNmethylated_0_thresh_WCGW <- as.numeric(lapply(strsplit(input$log_lik_ratio_repeated_WCGW, split = ","), get_count <- function(x, threshold) { return (sum(as.numeric(x) < threshold)) }, threshold = 0))
+
+
+
+    input %>%
+      group_by(read_name, chromosome) %>%
+      summarize(start = min(start),
+                end = max(end),
+    #            log_lik_ratio_collapsed = paste(log_lik_ratio_repeated, collapse=","),
+                total_CGs = sum(num_motifs),
+                total_CGs_methylated_2.5_thresh = sum(num_methylated_2.5_thresh),
+                total_CGs_methylated_0_thresh = sum(num_methylated_0_thresh),
+                  fraction_CGs_methylated_2.5_thresh = sum(num_methylated_2.5_thresh)/sum(num_motifs),
+                fraction_CGs_methylated_0_thresh = sum(num_methylated_0_thresh)/sum(num_motifs),
+                total_WCGW = sum(num_motifs_WCGW),
+                total_WCGW_methylated_2.5_thresh = sum(num_methylated_2.5_thresh_WCGW),
+                total_WCGW_methylated_0_thresh = sum(num_methylated_0_thresh_WCGW),
+                fraction_WCGW_methylated_2.5_thresh = sum(num_methylated_2.5_thresh_WCGW)/sum(num_motifs_WCGW),
+                fraction_WCGW_methylated_0_thresh = sum(num_methylated_0_thresh_WCGW)/sum(num_motifs_WCGW)) %>%
+      write_tsv(path=outfile_by_read)
+
+}
+
