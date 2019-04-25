06ccb0f84126a1ae1d39f208b31ce314310f2c64
Martin Aryee <martin.aryee@gmail.com>
2019-04-24 23:33:46 -0400

    add_flowcell_and_barcode_columns.R now gracefully handles cases with no output samples

--- Docker/nanopore_util/add_flowcell_and_barcode_columns.R
+++ Docker/nanopore_util/add_flowcell_and_barcode_columns.R
@@ -14,15 +14,21 @@ if (is.na(outfile))
 
 
 df <- read.csv(infile, stringsAsFactors = FALSE)
-
 if ("sample_id" %in% colnames(df)) 
-  stop("The input file already has a sample_id column")
+    stop("The input file already has a sample_id column")
 
-df <- df[,colnames(df) != "X"]
-x <- sub(".fq.gz", "", basename(df$fastq_gz))
-flowcell_and_barcode <- str_split_fixed(x, pattern = "__", n=2)
-colnames(flowcell_and_barcode) <- c("flowcell_id", "barcode_id")
-out <- cbind(sample_id="", flowcell_and_barcode, df)
-write.csv(out, outfile, quote=FALSE, row.names=FALSE)
 
-message("Wrote ", outfile)
+if (nrow(df) > 0) {
+    df <- df[,colnames(df) != "X"]
+    x <- sub(".fq.gz", "", basename(df$fastq_gz))
+    flowcell_and_barcode <- str_split_fixed(x, pattern = "__", n=2)
+    colnames(flowcell_and_barcode) <- c("flowcell_id", "barcode_id")
+    out <- cbind(sample_id="", flowcell_and_barcode, df)
+    write.csv(out, outfile, quote=FALSE, row.names=FALSE)
+    message("Wrote ", outfile)
+} else {
+    out <- data.frame(sample_id=character(), flowcell_id=character(), barcode_id=character(), df)
+    write.csv(out, outfile, quote=FALSE, row.names=FALSE)
+    message("Wrote ", outfile, " but there are no samples in it!")
+}
+
