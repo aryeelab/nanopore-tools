741255d2e17b595f49b49f46572fa44eeb6b5f28
Martin Aryee <martin.aryee@gmail.com>
2019-05-03 16:03:15 -0400

    Updated local singularity image path

--- pull_images.sh
+++ pull_images.sh
@@ -1,5 +1,5 @@
 module load local/singularity/2.5.2
-export SINGULARITY_CACHEDIR=/data/aryee/sowmya/ctc_nanopore/images
+export SINGULARITY_CACHEDIR=/data/aryee/simg
 (umask 002 && singularity pull docker://aryeelab/guppy)
 rm -rf  ${SINGULARITY_CACHEDIR}/docker/*
 rm -rf  ${SINGULARITY_CACHEDIR}/metadata/*
